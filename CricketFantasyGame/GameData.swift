49820248e179b37a309f1a3f0780957b63e7d237
PritiBanodha <pbanodha@equalexperts.com>
2018-01-08 17:29:00 +0530

    Initial test case

---
+++ CricketFantasyGame/GameData.swift
@@ -0,0 +1,14 @@
+//
+//  GameData.swift
+//  CricketFantasyGame
+//
+//  Created by Priti on 08/01/18.
+//  Copyright © 2018 EqualExperts. All rights reserved.
+//
+
+import Foundation
+
+class GameData
+{
+    let players = ["Rohit Sharma", "Virat Kohli", "Ravindra Jadeja", "Steven Smith", "David Warner", "Shane Watson"]
+}
